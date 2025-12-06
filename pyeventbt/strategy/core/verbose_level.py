25f7390d9fec5628f303a9ed60c46954b59d13ed
MCastany <marticastany@gmail.com>
2025-12-06 11:31:52 +0100

    Adds verbose level constants
    Introduces constants to represent different verbose levels,
    enhancing code readability and maintainability.
    These constants are used for filtering logs and controlling
    the amount of information displayed.

---
+++ pyeventbt/strategy/core/verbose_level.py
@@ -0,0 +1,20 @@
+"""
+PyEventBT
+Documentation: https://pyeventbt.com
+GitHub: https://github.com/marticastany/pyeventbt
+
+Author: Marti Castany
+Copyright (c) 2025 Marti Castany
+Licensed under the Apache License, Version 2.0
+"""
+
+
+class VerboseLevel(int):
+    CRITICAL = 50
+    FATAL = 50
+    ERROR = 40
+    WARNING = 30
+    WARN = 30
+    INFO = 20
+    DEBUG = 10
+    NOTSET = 0
